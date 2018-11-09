4ec6025f8e81a1939098ece846643713067dfc6e
Stephane Le Cornec <stephane.lecornec@gmail.com>
2018-11-09 15:02:28 -0500

    pos_line - add module

---
+++ pos_line/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Line',
+    'version': '1.0',
+    'author': 'Microcom',
+    'category': '',
+    'summary': '',
+    'description': """
+    """,
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/pos_order_view.xml',
+    ],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
