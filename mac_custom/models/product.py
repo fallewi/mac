6c8d0b31fbcb29c20b51518ac72cf7e496c170f8
Eric Lemire <el@microcom.ca>
2020-04-30 13:17:16 -0400

    Module to import image

--- mac_custom/models/product.py
+++ mac_custom/models/product.py
@@ -7,3 +7,9 @@ class SaleOrder(models.Model):
     _inherit = "product.uom"
 
     accept_decimal = fields.Boolean('Accept Decimal')
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    import_sequence = fields.Char('Import Sequence')
